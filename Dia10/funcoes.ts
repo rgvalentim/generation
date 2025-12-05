ee6eb1b082dce3f7cfc7ebbd2b6e85790332fcdf
Robson Valentim <rgvalentim@outlook.com>
2025-12-05 11:41:01 -0300

    Aula dia 10

---
+++ Dia10/funcoes.ts
@@ -0,0 +1,18 @@
+//Crie uma lógica em typescript que soma dois numeros e mostre todos os resultados no console
+//Sintax básica
+function nomeDaFuncao(parametro: number, parametro2: number): number{
+   //escopo local
+   return parametro - parametro2;
+}
+
+console.log(nomeDaFuncao(10, 50));
+//A: soma de 10 e 50
+let resultado = 10 + 50
+
+//B: soma de 30 e 60
+let resultado2 = 30 + 60
+
+//C soma de 70 + 80
+let resultado3 = 70 + 80
+
+console.log(resultado, resultado2, resultado3)
